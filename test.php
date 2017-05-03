80cf9c6a02ce55b762e75cb9451cad45d9063136
zsheill7 <zsheill7@gmail.com>
2017-05-02 22:59:23 -0700

    added test.php

--- test.php
+++ test.php
@@ -6,7 +6,7 @@
 
 	$headers = "From: katherine@gelsey.com";
 	if (mail($emailTo, $subject, $body, $headers)) {
-		echo "The email was sent";
+		echo "The email was sent2";
 	} else {
 		echo "The email could not be sent";
 	}
